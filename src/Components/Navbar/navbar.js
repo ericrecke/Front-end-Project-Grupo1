3a778160f0f56c99c41d16e1792422a1f4b5e8db
Ariel Farbiarz <arielfarbiarz@gmail.com>
2022-05-12 17:12:01 -0300

    Agrego componente navbar

---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Home</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Mascotas</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Personas</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Novedades</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+        );
+}
+
+export default Navbar;
