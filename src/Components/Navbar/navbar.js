230623823edc4cf7928e0724da45b8730d08fb47
Ariel Farbiarz <arielfarbiarz@gmail.com>
2022-05-12 18:12:35 -0300

    Mejora navbar, css para navbar, instalacion fontawesome

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
+import './navbar.css';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-light bg-success">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Home</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
