e785b0a4026837e117127b63325865e29b258e83
Ariel Farbiarz <arielfarbiarz@gmail.com>
2022-05-13 18:22:59 -0300

    Mejora navbar

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import './navbar.css';
+import { FaCat } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { AiTwotoneMail } from "react-icons/ai";
 
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-success">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Home</a>
+          <a class="navbar-brand" href="#"><FaCat class="cat"/></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -23,6 +28,20 @@ const Navbar = () => {
               </li>
               <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Contacto</a>
+              </li>
+            </ul>
+            <ul id="redes">
+              <li>
+              <a class="navbar-brand" href="#"><FaFacebook class="face"/></a>
+              </li>
+              <li>
+              <a class="navbar-brand" href="#"><FaInstagram class="insta"/></a>
+              </li>
+              <li>
+              <a class="navbar-brand" href="#"><FaTwitter class="twitter"/></a>
+              </li>
+              <li>
+              <a class="navbar-brand" href="#"><AiTwotoneMail class="mail"/></a>
               </li>
             </ul>
           </div>
