d2de2fc26954738196822b73666983579c44d17d
eric recke <ericrecke@hotmail.com>
2022-06-02 19:31:48 -0300

    Added Example Home to not let empty Index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/navbar.js';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/navbar.js";
+import Home from "./Components/Home/home";
 
 function App() {
-    return (
-      <Navbar/>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
